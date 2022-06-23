4d26ec43f6b51d82d8340e770bad3f06be0fab05
Durga Yamini <kodamanchiliyamini@gmail.com>
2022-06-23 22:23:23 +0530

    Add project files.

---
+++ Controllers/SongsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicRestAPI.Data;
+using MusicRestAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private readonly SongsDbContext _dbContext;
+
+        public SongsController(SongsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IEnumerable<Song> GetAllSongs()
+        {
+            return _dbContext.Songs.ToList();
+        }
+        [HttpGet("{id}")]
+        public Song GetSongById(int id)
+        {
+            return _dbContext.Songs.Find(id);
+        }
+        [HttpPost]
+        public void AddSong([FromBody]Song song)
+        {
+            _dbContext.Songs.Add(song);
+            _dbContext.SaveChanges();
+        }
+        [HttpPut("{id}")]
+        public void UpdateSong(int id, [FromBody]Song song)
+        {
+            var existing = _dbContext.Songs.Find(id);
+            existing.Title = song.Title;
+            existing.Language = song.Language;
+            _dbContext.SaveChanges();
+        }
+        [HttpDelete("{id}")]
+        public void DeleteSong(int id)
+        {
+            var existing = _dbContext.Songs.Find(id);
+            _dbContext.Songs.Remove(existing);
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Data/SongsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicRestAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRestAPI.Data
+{
+    public class SongsDbContext : DbContext
+    {
+        public SongsDbContext(DbContextOptions<SongsDbContext> options) : base(options) { }
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
